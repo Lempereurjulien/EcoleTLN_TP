beedd7e0150cf1390a110b5bc3d0f554876c9da0
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-03-23 09:03:45 +0100

    class fait : Etudiant, EtudiantChange,Personnel

--- EcoleTLN_TP/Contact.cs
+++ EcoleTLN_TP/Contact.cs
@@ -8,14 +8,53 @@ namespace Classes.ClassesEcole
 {
     class Contact
     {
+        private int matricule;
+        private string nom;
+        private int anneeArrivee;
+
         public Contact( int matricule, string nom, int anneeArrivee)
         {
-           
-        }
-       
+            this.matricule = matricule;
+            this.nom = nom;
+            this.anneeArrivee = anneeArrivee;
+        }       
     }
-    class Etudiant : Contact
-    {
+
+    class Etudiant :Contact
+    { 
         private string section;
+        public Etudiant(int matricule, string nom, int anneeArrivee, string section):base(matricule,nom,anneeArrivee) 
+        {
+            this.section = section;
+        }
     }
+
+    class EtudiantEchange : Etudiant
+    {
+        public string universiteOrigine; 
+        public EtudiantEchange(int matricule, string nom, int anneeArrivee, string section, string universiteOrigine) : base(matricule, nom, anneeArrivee, section)
+        {
+            this.universiteOrigine = universiteOrigine;
+        }
+    }
+    class EtudiantRegulier:Etudiant
+    {
+        public double noteMoyenne;
+        EtudiantRegulier(int matricule, string nom, int anneeArrivee, string section, double noteMoyenne) : base(matricule, nom, anneeArrivee, section)
+        {
+            this.noteMoyenne = noteMoyenne;
+        }
+    }
+
+    class Personnel:Contact
+    {
+        private string nomLaboratoire;
+        private double salaire;
+        public Personnel(int matricule, string nom, int anneeArrivee, string nomLaboratoire, double salaire) : base(matricule, nom, anneeArrivee)
+        {
+            this.nomLaboratoire = nomLaboratoire;
+            this.salaire = salaire;
+        }
+    }
+
 }
