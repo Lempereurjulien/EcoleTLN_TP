d9a97a4dc1025738536dbe965c2dd29bea6ec77d
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-03-23 08:38:43 +0100

    Ajoutez des fichiers projet.

---
+++ EcoleTLN_TP/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.ClassesEcole
+{
+    class Contact
+    {
+        public Contact( int matricule, string nom, int anneeArrivee)
+        {
+           
+        }
+       
+    }
+    class Etudiant : Contact
+    {
+        private string section;
+    }
+}
---
+++ EcoleTLN_TP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoleTLN_TP
+{
+    class EcoleTLN
+    {
+        private string nom;
+        private char anneeCreation;
+        static void Main()
+        {
+        }   
+    }
+}
